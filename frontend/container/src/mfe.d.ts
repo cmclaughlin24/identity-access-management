ccb5c97d1effd655f195d959e6fe2ef1f2f6066a
cmclaughlin24 <curtismclaughlin91@gmail.com>
2026-03-05 18:32:30 -0600

    add route to container

--- frontend/container/src/mfe.d.ts
+++ frontend/container/src/mfe.d.ts
@@ -1,3 +1,7 @@
 declare module "authentication/Routes" {
   export const federationRoutes: import("react-router-dom").RouteObject[];
 }
+
+declare module "certification/Routes" {
+  export const federationRoutes: import("react-router-dom").RouteObject[];
+}
