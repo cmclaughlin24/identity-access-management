e4960dcf5fdf585e0695486e48e1d74c5d19ee2b
cmclaughlin24 <curtismclaughlin91@gmail.com>
2026-03-06 13:07:53 -0600

    set-up tracing

---
+++ frontend/certification/src/telemetry.ts
@@ -0,0 +1,33 @@
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+
+const OTEL_ENDPOINT = 'http://localhost:4318/v1/traces';
+
+export function initTelemetry() {
+  const exporter = new OTLPTraceExporter({
+    url: OTEL_ENDPOINT,
+  });
+
+  const provider = new WebTracerProvider({
+    resource: resourceFromAttributes({
+      [SEMRESATTRS_SERVICE_NAME]: 'certification',
+    }),
+    spanProcessors: [new BatchSpanProcessor(exporter)],
+  });
+
+  provider.register({
+    contextManager: new ZoneContextManager(),
+  });
+
+  registerInstrumentations({
+    instrumentations: [
+      getWebAutoInstrumentations(),
+    ],
+  });
+}
